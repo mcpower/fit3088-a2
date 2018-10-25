1fdefd44279fb6e6575efa647208e39582053780
Michael <mcpower@users.noreply.github.com>
2018-10-25 17:43:36 +1100

    update Buffer

--- src/classes/Buffer.ts
+++ src/classes/Buffer.ts
@@ -3,11 +3,19 @@ export default class Buffer {
     buffer: WebGLBuffer;
     // gl.ARRAY_BUFFER, gl.ELEMENT_ARRAY_BUFFER, etc.
     target: GLenum;
-    // gl.FLOAT, gl.UNSIGNED_INT, etc.
-    type: GLenum;
-    size?: GLint;
+    // gl.FLOAT, etc.
+    type?: GLenum;
+    size?: 1 | 2 | 3 | 4;
 
-    constructor(gl: WebGLRenderingContext, data: ArrayBuffer | ArrayBufferView, target: GLenum, type: GLenum, size?: GLint) {
+    /**
+     * Constructor.
+     * @param gl The current rendering context.
+     * @param data The data to put in the buffer.
+     * @param target The binding point (like gl.ARRAY_BUFFER, gl.ELEMENT_ARRAY_BUFFER)
+     * @param type What the type of the buffer is (gl.FLOAT, gl.UNSIGNED_INT, etc.)
+     * @param size Number of components per vertex attribute (if using as vertex buffer).
+     */
+    constructor(gl: WebGLRenderingContext, data: ArrayBuffer | ArrayBufferView, target: GLenum, type?: GLenum, size?: 1 | 2 | 3 | 4) {
         this.gl = gl;
         const buffer = gl.createBuffer();
         if (buffer === null) {
@@ -30,6 +38,9 @@ export default class Buffer {
     initAttrib(attrib: GLuint) {
         if (this.size === undefined) {
             throw new Error("need to define size");
+        }
+        if (this.type === undefined) {
+            throw new Error("need to define type");
         }
         this.bind();
         this.gl.vertexAttribPointer(attrib, this.size, this.type, false, 0, 0);

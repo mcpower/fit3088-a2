a298b82e7059ae58604ea0564cecf58c47d600d3
Michael <mcpower@users.noreply.github.com>
2018-10-25 18:13:26 +1100

    begin rendering in Context

--- src/classes/Context.ts
+++ src/classes/Context.ts
@@ -1,5 +1,6 @@
 import Program from "./Program";
 import WebGLUtils from "../lib/webgl-utils";
+import { Matrix } from "../lib/MV";
 
 /**
  * The WebGLRenderingContext and all related rendering.
@@ -9,9 +10,26 @@ export default class Context {
     gl: WebGLRenderingContext;
     programs: Program[];
 
+    model: Matrix;
+    view: Matrix;
+    projection: Matrix;
+
     constructor(gl: WebGLRenderingContext) {
         this.gl = gl;
         this.programs = [];
+    }
+
+    render = () => {
+        const gl = this.gl;
+
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); // Clear color and depth buffers
+
+        this.programs.forEach(prog => {
+            gl.useProgram(prog.program);
+            prog.render(this.model, this.view, this.projection);
+        });
+
+        window.requestAnimationFrame(this.render);
     }
 
     static fromCanvas(canvas: HTMLCanvasElement) {

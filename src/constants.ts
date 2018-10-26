9abd4b3abd2ad1330840ed64518c0d8194553351
Michael <mcpower@users.noreply.github.com>
2018-10-26 19:43:35 +1100

    Make selected satellite red

--- src/constants.ts
+++ src/constants.ts
@@ -2,13 +2,13 @@ export const EARTH_RADIUS_KM = 6371;
 export const EARTH_RADIUS_GL = 0.18;
 export const EPS = 1e-9;
 // How much to scale the satellites by.
-export const SATELLITE_SCALE = 200;
+export const SATELLITE_SCALE = 150;
 // How big the satellites are when picking them.
 export const SATELLITE_PICK_RADIUS = 2000;
 
 export const SATELLITE_DIFFUSE = [0.4, 0.4, 0.4];
 export const SATELLITE_AMBIENT = [0.4, 0.4, 0.4];
-export const SATELLITE_AMBIENT_SELECTED = [0.8, 0.8, 0.8];
+export const SATELLITE_AMBIENT_SELECTED = [0.8, 0.2, 0.2];
 
 export const ORBIT_COLOR = [0.2, 0.2, 0.2];
-export const ORBIT_COLOR_SELECTED = [0.9, 0.9, 0.9];
+export const ORBIT_COLOR_SELECTED = [0.9, 0.2, 0.2];
